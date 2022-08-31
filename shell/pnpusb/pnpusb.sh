f94246fe08cbc853b413a756f03a199d4864ad42
xinli <xinligg@gmail.com>
2022-08-31 21:14:11 +0800

    加入热拔插USB设备脚本

---
+++ shell/pnpusb/pnpusb.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+ 
+bind_usb() {
+  echo -n "$1" | tee /sys/bus/pci/drivers/ehci-pci/bind
+}
+ 
+unbind_usb() {
+  echo -n "$1" | tee /sys/bus/pci/drivers/ehci-pci/unbind
+}
+ 
+DEVICE_NUM=`lspci |grep USB|awk -F' ' '{print$1}'|wc -l`
+ 
+for ((i=1; i <= $DEVICE_NUM; i++))
+do
+        DEVICE=`lspci |grep USB|awk -F' ' '{print "0000:"$1}'|sed -n ${i}p`
+        unbind_usb $DEVICE
+        bind_usb $DEVICE
+done
